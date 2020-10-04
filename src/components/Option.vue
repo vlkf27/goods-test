ee737ec81213cadb73e9b997f8f92f4c4ed5d1c6
vlkf27 <vlkf27@users.noreply.github.com>
2020-10-04 23:24:57 +0300

    feat(select): base component styled

---
+++ src/components/Option.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .option
+    slot
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Option extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.option {
+  padding: 12px 15px;
+  background: #fff;
+  cursor: pointer;
+  &:hover {
+    background: #f6f6f6;
+  }
+}
+</style>
---
+++ src/components/Select.vue
@@ -0,0 +1,100 @@
+<template lang="pug">
+  .select(
+    :class="{ opened }",
+    @click.stop="open"
+  )
+    .placeholder Pick one
+    img(class="arrow", src="@/assets/down-arrow.svg")
+    .items
+      Option First
+      Option Second
+      Option Third
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Option from "@/components/Option.vue";
+
+@Component({
+  components: {
+    Option
+  }
+})
+export default class Select extends Vue {
+  opened = false;
+
+  open() {
+    this.opened = true;
+  }
+  close() {
+    this.opened = false;
+  }
+
+  mounted() {
+    this.addDocumentListeners();
+  }
+
+  destroyed() {
+    this.removeDocumentListeners();
+  }
+
+  removeDocumentListeners() {
+    document.removeEventListener("click", this.documentClickHandler);
+  }
+
+  addDocumentListeners() {
+    document.addEventListener("click", this.documentClickHandler);
+  }
+
+  documentClickHandler() {
+    this.close();
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$selectRadius: 3px;
+
+.select {
+  background: #fff;
+  cursor: pointer;
+  width: 100%;
+  padding: 15px 10px;
+  border-radius: $selectRadius;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.05);
+  display: flex;
+  position: relative;
+  &.opened {
+    .items {
+      display: flex;
+    }
+    .arrow {
+      transform: translateY(-50%) rotate(-180deg);
+    }
+  }
+}
+.arrow {
+  position: absolute;
+  right: 15px;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 15px;
+  height: auto;
+  transition: all 300ms;
+}
+.items {
+  display: none;
+  position: absolute;
+  top: calc(100% - 3px);
+  background: #fff;
+  height: auto;
+  max-height: 300px;
+  width: 100%;
+  left: 0;
+  flex-direction: column;
+  overflow: auto;
+
+  border-bottom-right-radius: $selectRadius;
+  border-bottom-left-radius: $selectRadius;
+}
+</style>
